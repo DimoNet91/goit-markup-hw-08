e0ec59442ceda427a37322f80814db738c8525a9
Dmitriy <98262286+DimoNet91@users.noreply.github.com>
2022-07-09 00:07:32 +0300

    add upd 08.07.2022

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.btn-menu-mob-op'),
+    closeMenuBtn: document.querySelector('.btn-menu-mob-close'),
+    menu: document.querySelector('.mob-menu'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+})();
